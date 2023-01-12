663a025a780af1596f55bffd53186d7909ae7f5b
TJ <xxterrelljacksonxx@gmail.com>
2023-01-12 12:47:33 -0500

    updated tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,23 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
- darkMode: false, // or 'media' or 'class'
- theme: {
-   extend: {},
- },
- variants: {
-   extend: {
-       scale: ['active'] // to use the active: pseudoselector
-   },
- },
- plugins: [],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    scale: ['active'] // to use the active: pseudoselector
+  },
+  plugins: [],
 }
+
+// module.exports = {
+//   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+//  darkMode: false, // or 'media' or 'class'
+//  theme: {
+//    extend: {},
+//  },
+//  variants: {
+//    extend: {
+//        scale: ['active'] // to use the active: pseudoselector
+//    },
+//  },
+//  plugins: [],
+// }
