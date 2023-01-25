1e18524b4bfec44c3ccc14ab4404820f71179c75
TJ <xxterrelljacksonxx@gmail.com>
2023-01-25 18:54:53 -0500

    rank update

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -2,7 +2,7 @@ const Rank = ({name, entries}) => {
     return (
         <div>
             <div className="white f3">
-                {`Hello ${name}, your current entry count is...`}
+                {`Hello ${name}, your total number image uploads is...`}
             </div>
             <div className="white f1">
                 {entries}
