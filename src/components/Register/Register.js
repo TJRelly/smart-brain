ac0134e6c4dc48bcee33a0448897f9f5fe71293b
TJ <xxterrelljacksonxx@gmail.com>
2023-04-19 00:07:05 -0400

    added loading screen

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../SignIn/Siginin.css'
+import LoadingScreen from 'react-loading-screen'
 
 class Register extends React.Component {
     constructor(props) {
@@ -53,55 +54,63 @@ class Register extends React.Component {
     render() {
         const { onRouteChange } = this.props
         return (
-            <form onSubmit={this.onFormSubmit} id='signin-form' className="bg-grey-lighter flex flex-col sm:w-full h-auto m-auto">
-                <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                    <div className="px-6 py-8 rounded shadow-xl text-black w-full">
-                        <h1 className="text-white mb-8 text-3xl text-center">Create Account</h1>
+            <LoadingScreen
+                loading={this.state.loading}
+                bgColor='transparent'
+                spinnerColor='#ffffff'
+                textColor='#ffffff'
+                logoSrc=''
+                text='Loading your content...'
+            ><form onSubmit={this.onFormSubmit} id='signin-form' className="bg-grey-lighter flex flex-col sm:w-full h-auto m-auto">
+                    <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                        <div className="px-6 py-8 rounded shadow-xl text-black w-full">
+                            <h1 className="text-white mb-8 text-3xl text-center">Create Account</h1>
 
-                        <input
-                            type="text"
-                            className="block border border-grey-light w-full p-3 rounded mb-4"
-                            name="user-id"
-                            placeholder="Name"
-                            onChange={this.onNameChange}
-                        />
+                            <input
+                                type="text"
+                                className="block border border-grey-light w-full p-3 rounded mb-4"
+                                name="user-id"
+                                placeholder="Name"
+                                onChange={this.onNameChange}
+                            />
 
-                        <input
-                            type="text"
-                            className="block border border-grey-light w-full p-3 rounded mb-4"
-                            name="email"
-                            placeholder="Email"
-                            onChange={this.onEmailChange}
-                        />
+                            <input
+                                type="text"
+                                className="block border border-grey-light w-full p-3 rounded mb-4"
+                                name="email"
+                                placeholder="Email"
+                                onChange={this.onEmailChange}
+                            />
 
-                        <input
-                            type="password"
-                            className="block border border-grey-light w-full p-3 rounded mb-4"
-                            name="password"
-                            placeholder="Password"
-                            onChange={this.onPasswordChange}
-                        />
+                            <input
+                                type="password"
+                                className="block border border-grey-light w-full p-3 rounded mb-4"
+                                name="password"
+                                placeholder="Password"
+                                onChange={this.onPasswordChange}
+                            />
 
-                        <button
-                            id='submit-form'
-                            type="submit"
-                            className="white w-full text-center py-3 rounded bg-blue-600 text-gray-800 hover:text-white focus:text-white hover:bg-green-dark focus:outline-none my-1"
-                        >{this.state.loading ? <> Loading . . .</> : <>Sign up</>}
-                        </button>
-                        <p className='development'> - Thank you for trying our app! - </p>
+                            <button
+                                id='submit-form'
+                                type="submit"
+                                className="white w-full text-center py-3 rounded bg-blue-600 text-gray-800 hover:text-white focus:text-white hover:bg-green-dark focus:outline-none my-1"
+                            >{this.state.loading ? <>Signing in . . .</> : <>Sign up</>}
+                            </button>
+                            <p className='development'> - Thank you for trying our app! - </p>
+                        </div>
+
+                        <div className="text-grey-dark mt-6 bg-white px-2 py-5 rounded-lg shadow-md text-black w-full">
+                            Already have an account?
+                            <button
+                                className="text-lg no-underline border-b text-blue-700 transition duration-200 hover:border-blue-700 text-blue ml-2"
+                                href="../login/"
+                                onClick={() => onRouteChange('signin')}>
+                                Log in.
+                            </button>
+                        </div>
                     </div>
-
-                    <div className="text-grey-dark mt-6 bg-white px-2 py-5 rounded-lg shadow-md text-black w-full">
-                        Already have an account?
-                        <button
-                            className="text-lg no-underline border-b text-blue-700 transition duration-200 hover:border-blue-700 text-blue ml-2"
-                            href="../login/"
-                            onClick={() => onRouteChange('signin')}>
-                            Log in.
-                        </button>
-                    </div>
-                </div>
-            </form>
+                </form>
+            </LoadingScreen>
         )
     }
 }
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Siginin.css'
+import LoadingScreen from 'react-loading-screen'
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -47,46 +48,55 @@ class SignIn extends React.Component {
     render() {
         const { onRouteChange } = this.props
         return (
-            <form onSubmit={this.onFormSubmit} id='signin-form' className="bg-grey-lighter flex flex-col sm:w-full h-auto m-auto">
-                <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                    <div className="px-6 py-8 rounded shadow-xl text-black w-full">
-                        <h1 className="text-white mb-0 text-3xl text-center"> - Smart Brain - </h1>
-                        <h1 className="text-white mb-8 mt-0 text-2xl text-center">Face-Recognition App</h1>
-                        <input
-                            onChange={this.onEmailChange}
-                            type="text"
-                            className="block border border-grey-light w-full p-3 rounded mb-4"
-                            name="email"
-                            placeholder="Email" />
+            <LoadingScreen
+                loading={this.state.loading}
+                bgColor='transparent'
+                spinnerColor='#ffffff'
+                textColor='#ffffff'
+                logoSrc=''
+                text='Loading your content...'
+            >
+                <form onSubmit={this.onFormSubmit} id='signin-form' className="bg-grey-lighter flex flex-col sm:w-full h-auto m-auto">
+                    <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                        <div className="px-6 py-8 rounded shadow-xl text-black w-full">
+                            <h1 className="text-white mb-0 text-3xl text-center"> - Smart Brain - </h1>
+                            <h1 className="text-white mb-8 mt-0 text-2xl text-center">Face-Recognition App</h1>
+                            <input
+                                onChange={this.onEmailChange}
+                                type="text"
+                                className="block border border-grey-light w-full p-3 rounded mb-4"
+                                name="email"
+                                placeholder="Email" />
 
-                        <input
-                            onChange={this.onPasswordChange}
-                            type="password"
-                            className="block border border-grey-light w-full p-3 rounded mb-4"
-                            name="password"
-                            placeholder="Password" />
+                            <input
+                                onChange={this.onPasswordChange}
+                                type="password"
+                                className="block border border-grey-light w-full p-3 rounded mb-4"
+                                name="password"
+                                placeholder="Password" />
 
-                        <button
-                            id='submit-form'
-                            type="submit"
-                            className="white w-full text-center py-3 rounded bg-blue-600 text-gray-800 hover:text-white focus:text-white hover:bg-green-dark focus:outline-none my-1"
-                        >{this.state.loading ? <> Loading . . .</> : <>Log in</>}
-                        </button>
-                        <p className='development'>- Guest Log in -</p>
-                        <p className='development'>email: guest@gmail.com &nbsp;&nbsp; Password: 123</p>
+                            <button
+                                id='submit-form'
+                                type="submit"
+                                className="white w-full text-center py-3 rounded bg-blue-600 text-gray-800 hover:text-white focus:text-white hover:bg-green-dark focus:outline-none my-1"
+                            >{this.state.loading ? <>Logging in . . .</> : <>Log in</>}
+                            </button>
+                            <p className='development'>- Guest Log in -</p>
+                            <p className='development'>email: guest@gmail.com &nbsp;&nbsp; Password: 123</p>
+                        </div>
+
+                        <div className="text-grey-dark mt-6 bg-white px-2 py-5 rounded-lg shadow-md text-black w-full">
+                            Don't have an account?
+                            <button
+                                className="text-lg no-underline border-b text-blue-700 transition duration-200 hover:border-blue-700 text-blue ml-2"
+                                href="../login/"
+                                onClick={() => onRouteChange('register')}>
+                                Create one!
+                            </button>
+                        </div>
                     </div>
-
-                    <div className="text-grey-dark mt-6 bg-white px-2 py-5 rounded-lg shadow-md text-black w-full">
-                        Don't have an account?
-                        <button
-                            className="text-lg no-underline border-b text-blue-700 transition duration-200 hover:border-blue-700 text-blue ml-2"
-                            href="../login/"
-                            onClick={() => onRouteChange('register')}>
-                            Create one!
-                        </button>
-                    </div>
-                </div>
-            </form >
+                </form >
+            </LoadingScreen>
         )
     }
 }
