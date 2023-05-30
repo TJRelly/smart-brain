86570648f063d437730c863a48901f4c4eb246b4
TJ <xxterrelljacksonxx@gmail.com>
2023-05-30 18:50:41 -0400

    name update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import ParticlesBg from 'particles-bg'
-import SignIn from './components/SignIn/SignIn';
-import Register from './components/Register/Register';
+import React, { Component } from "react"
+import "./App.css"
+import Navigation from "./components/Navigation/Navigation"
+import Logo from "./components/Logo/Logo"
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm"
+import Rank from "./components/Rank/Rank"
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition"
+import ParticlesBg from "particles-bg"
+import SignIn from "./components/SignIn/SignIn"
+import Register from "./components/Register/Register"
 
 const initialState = {
-  input: '',
-  imageUrl: '',
+  input: "",
+  imageUrl: "",
   boxes: [],
-  route: 'signin',
+  route: "signin",
   isSignedIn: false,
   user: {
-    id: '',
-    name: '',
-    email: '',
+    id: "",
+    name: "",
+    email: "",
     entries: 0,
-    joined: ''
-  }
+    joined: "",
+  },
 }
 
 class App extends Component {
   constructor() {
-    super();
-    this.state = initialState;
+    super()
+    this.state = initialState
   }
 
   loadUser = (data) => {
@@ -37,45 +37,43 @@ class App extends Component {
         name: data.name,
         email: data.email,
         entries: data.entries,
-        joined: data.joined
-      }
+        joined: data.joined,
+      },
     })
   }
 
   componentDidMount() {
-    fetch('https://smart-brain-api-z87p.onrender.com/')
-      .then(response => response.json())
+    fetch("https://smart-brain-api-z87p.onrender.com/").then((response) =>
+      response.json()
+    )
   }
 
   onRouteChange = (route) => {
-    if (route === 'signout') {
+    if (route === "signout") {
       this.setState(initialState)
-    } else if (route === 'home') {
+    } else if (route === "home") {
       this.setState({ isSignedIn: true })
     }
     this.setState({ route: route })
   }
 
-  calculateFaceLocation = data => {
-    const image = document.getElementById('inputimage');
-    const width = Number(image.width);
-    const height = Number(image.height);
-    return JSON
-      .parse(data, null, 2)
-      .outputs[0].data.regions
-      .map(face => {
-        const clarifaiFace = face.region_info.bounding_box;
-        return {
-          leftCol: clarifaiFace.left_col * width,
-          topRow: clarifaiFace.top_row * height,
-          rightCol: width - clarifaiFace.right_col * width,
-          bottomRow: height - clarifaiFace.bottom_row * height,
-        };
-      });
-  };
+  calculateFaceLocation = (data) => {
+    const image = document.getElementById("inputimage")
+    const width = Number(image.width)
+    const height = Number(image.height)
+    return JSON.parse(data, null, 2).outputs[0].data.regions.map((face) => {
+      const clarifaiFace = face.region_info.bounding_box
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - clarifaiFace.right_col * width,
+        bottomRow: height - clarifaiFace.bottom_row * height,
+      }
+    })
+  }
 
   displayFaceBox = (boxes) => {
-    this.setState({ boxes: boxes });
+    this.setState({ boxes: boxes })
   }
 
   onInputChange = (event) => {
@@ -86,69 +84,66 @@ class App extends Component {
     this.setState({ imageUrl: this.state.input })
 
     const raw = JSON.stringify({
-      "user_app_id": {
-        "user_id": "clarifai",
-        "app_id": "main"
+      user_app_id: {
+        user_id: "clarifai",
+        app_id: "main",
       },
-      "inputs": [
+      inputs: [
         {
-          "data": {
-            "image": {
-              "url": this.state.input
-            }
-          }
-        }
-      ]
-    });
+          data: {
+            image: {
+              url: this.state.input,
+            },
+          },
+        },
+      ],
+    })
 
     const requestOptions = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Accept': 'application/json',
-        'Authorization': 'Key dc909b76df6646348782f475407e883b'
+        Accept: "application/json",
+        Authorization: "Key dc909b76df6646348782f475407e883b",
       },
-      body: raw
-    };
+      body: raw,
+    }
 
-    fetch(`https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`, requestOptions)
-      .then(response => response.text())
-      .then(result => {
+    fetch(
+      `https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`,
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => {
         console.log(result)
         this.displayFaceBox(this.calculateFaceLocation(result))
         if (result) {
-          fetch('https://smart-brain-api-z87p.onrender.com/image', {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
+          fetch("https://smart-brain-api-z87p.onrender.com/image", {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-              id: this.state.user.id
-            })
+              id: this.state.user.id,
+            }),
           })
-            .then(result => result.json())
-            .then(count => {
+            .then((result) => result.json())
+            .then((count) => {
               this.setState(Object.assign(this.state.user, { entries: count }))
             })
         }
       })
-      .catch(error => console.log('error', error));
-
+      .catch((error) => console.log("error", error))
   }
 
   render() {
-    const { isSignedIn, imageUrl, route, boxes } = this.state;
+    const { isSignedIn, imageUrl, route, boxes } = this.state
     return (
-      <div className="App" >
-        <ParticlesBg
-          color="#FFFFFF"
-          num={100}
-          type="cobweb"
-          bg={true}
-        />
+      <div className="App">
+        <ParticlesBg color="#FFFFFF" num={100} type="cobweb" bg={true} />
         <Navigation
           isSignedIn={isSignedIn}
           onRouteChange={this.onRouteChange}
         />
-        {route === 'home'
-          ? <>
+        {route === "home" ? (
+          <>
             <Logo />
             <Rank
               name={this.state.user.name}
@@ -158,26 +153,19 @@ class App extends Component {
               onInputChange={this.onInputChange}
               onSubmit={this.onSubmit}
             />
-            <FaceRecognition
-              boxes={boxes}
-              imageUrl={imageUrl}
-            />
+            <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
           </>
-          : (
-            route !== 'register'
-              ? <SignIn
-                loadUser={this.loadUser}
-                onRouteChange={this.onRouteChange}
-              />
-              : <Register
-                loadUser={this.loadUser}
-                onRouteChange={this.onRouteChange}
-              />
-          )
-        }
+        ) : route !== "register" ? (
+          <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
+        )}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
