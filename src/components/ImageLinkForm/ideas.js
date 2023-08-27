106864f959d547e4202488ee13fb543bc96caa11
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2023-08-26 22:18:41 -0400

    security

---
+++ src/components/ImageLinkForm/ideas.js
@@ -0,0 +1,23 @@
+;<div className="mx-auto w-fit">
+  <p className="text-2xl mt-6 text-white backdrop-blur-sm w-fit">
+    {"Instructions"}
+  </p>
+  <p className="f4 mt-3 text-white backdrop-blur-sm w-fit">
+    {'1. Google "faces"'}
+  </p>
+  <p className="f4 mt-3 text-white backdrop-blur-sm w-fit">
+    {"2. Choose an image"}
+  </p>
+  <p className="f4 mt-3 text-white backdrop-blur-sm w-fit">
+    {"3. Right click and"}{" "}
+    <span
+      className="border-b-2 border-white
+  "
+    >
+      {"copy image address."}
+    </span>
+  </p>
+  <p className="f4 mt-3 text-white backdrop-blur-sm w-fit">
+    {"4. Click Detect"}
+  </p>
+</div>
